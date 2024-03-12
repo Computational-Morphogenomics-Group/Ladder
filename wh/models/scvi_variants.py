1d4be77cf7b8e4da8db1a0da2da35e7907de0650
zgr2788 <ozgur.beker@gmail.com>
2024-03-12 09:18:57 -0400

    add normal vae

--- wh/models/scvi_variants.py
+++ wh/models/scvi_variants.py
@@ -9,7 +9,89 @@ import pyro.poutine as poutine
 from.basics import broadcast_inputs, make_func
 import numpy as np
 
+#================================================================================================
+#================================================================================================
+#================================================================================================
+#================================================================================================
+#================================================================================================
+#================================================================================================
 
+## scANVI taken from https://pyro.ai/examples/scanvi.html
+class SCVI(nn.Module):
+    """
+    SCVI
+    """
+    
+    def __init__(self, num_genes, l_loc, l_scale, hidden_dim=128, num_layers=1,
+                 latent_dim=10, scale_factor=1.0):
+         
+
+        # Init params & hyperparams
+        self.scale_factor = scale_factor
+        self.num_genes = num_genes
+        self.latent_dim = latent_dim
+        self.l_loc = l_loc
+        self.l_scale = l_scale
+
+
+
+        super(SCVI, self).__init__()
+
+        # Setup NN functions
+        self.z_decoder = make_func(in_dims=self.latent_dim, hidden_dims=[hidden_dim]*num_layers, out_dim=self.latent_dim, last_config="reparam", dist_config="normal")
+        self.x_decoder = make_func(in_dims=self.latent_dim, hidden_dims=[hidden_dim]*num_layers, out_dim=self.num_genes, last_config="reparam", dist_config="zinb")
+        self.zl_encoder = make_func(in_dims=self.num_genes, hidden_dims=[hidden_dim]*num_layers, out_dim=self.latent_dim, last_config="+lognormal", dist_config="+lognormal")
+        
+
+        self.epsilon = 0.006
+
+    # Model
+    def model(self, x, y=None):
+        pyro.module("scvi", self)
+
+        # Inverse dispersions
+        theta = pyro.param("inverse_dispersion", 10.0 * x.new_ones(self.num_genes), constraint=constraints.positive)
+
+        
+        with pyro.plate("batch", len(x)), poutine.scale(scale=self.scale_factor):
+            z = pyro.sample("z", dist.Normal(0, x.new_ones(self.latent_dim)).to_event(1))
+            
+            l_loc, l_scale = self.l_loc * x.new_ones(1), self.l_scale * x.new_ones(1)
+            l = pyro.sample("l", dist.LogNormal(l_loc, l_scale).to_event(1))
+
+            
+            gate_logits, mu = self.x_decoder(z)
+            nb_logits = (l * mu + self.epsilon).log() - (theta + self.epsilon).log()
+            x_dist = dist.ZeroInflatedNegativeBinomial(gate_logits=gate_logits, total_count=theta,
+                                                       logits=nb_logits, validate_args=False)
+            
+            
+            pyro.sample("x", x_dist.to_event(1), obs=x)
+
+    
+    # Guide
+    def guide(self, x, y=None):
+        pyro.module("scvi", self)
+        
+        with pyro.plate("batch", len(x)), poutine.scale(scale=self.scale_factor):
+            
+            z_loc, z_scale, l_loc, l_scale = self.zl_encoder(x)
+            
+            pyro.sample("l", dist.LogNormal(l_loc, l_scale).to_event(1))
+            pyro.sample("z", dist.Normal(z_loc, z_scale).to_event(1))
+
+
+    # Save self
+    def save(self, path="scvi_params"):
+        torch.save(self.state_dict(), path + "_torch.pth")
+        pyro.get_param_store().save(path + "_pyro.pth")
+
+
+    # Load
+    def load(self, path="scvi_params"):
+        pyro.clear_param_store()
+        self.load_state_dict(torch.load(path + "_torch.pth"))
+        pyro.get_param_store().load(path + "_pyro.pth")
 
 
 #================================================================================================
@@ -102,6 +184,45 @@ class SCANVI(nn.Module):
             z2_y = torch.cat(z2_y, dim=-1)
             z1_loc, z1_scale = self.z1_encoder(z2_y)
             pyro.sample("z1", dist.Normal(z1_loc, z1_scale).to_event(1))
+
+
+    # Function to move points between conditions
+    @torch.no_grad()
+    def generate(self, x, y):
+        pyro.module("scanvi", self)
+  
+        ## Encode
+        #Variational for rho & l
+        z2_loc, z2_scale, l_loc, l_scale = self.z2l_encoder(x)
+            
+        l_enc = pyro.sample("l_enc", dist.LogNormal(l_loc, l_scale).to_event(1))
+        z2_enc = pyro.sample("z2_enc", dist.Normal(z2_loc, z2_scale).to_event(1))
+
+
+        # Variational for w & z
+        z2_y = broadcast_inputs([z2, y])
+        z2_y = torch.cat(z2_y, dim=-1)
+        z1_loc, z1_scale = self.z1_encoder(z2_y)
+        z1_enc = pyro.sample("z1", dist.Normal(z1_loc, z1_scale).to_event(1))
+
+        
+        ## Decode
+        theta = dict(pyro.get_param_store())["inverse_dispersion"].detach()
+
+        z1_y = torch.cat([z1_enc, y], dim=-1)
+        z2_loc, z2_scale = self.z2_decoder(z1_y)
+        z2 = pyro.sample("z2", dist.Normal(z2_loc, z2_scale).to_event(1))
+
+
+        gate_logits, mu = self.x_decoder(z2)
+        nb_logits = (l_enc * mu + self.epsilon).log() - (theta + self.epsilon).log()
+        x_dist = dist.ZeroInflatedNegativeBinomial(gate_logits=gate_logits, total_count=theta,
+                                                       logits=nb_logits, validate_args=False)
+            
+        #Observe the datapoint x using the observation distribution x_dist
+        x_rec = pyro.sample("x", x_dist.to_event(1))
+
+        return x_rec
 
 
     # Save self
