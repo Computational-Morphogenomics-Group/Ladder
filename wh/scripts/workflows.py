29662bade66b09bdccd4a0c4aabf8ae78b8d8655
zgr2788 <ozgur.beker@gmail.com>
2024-09-05 17:10:12 -0400

    implement high level workflows up until training loop

--- wh/scripts/workflows.py
+++ wh/scripts/workflows.py
@@ -5,13 +5,18 @@ import anndata as ad
 import scanpy as sc
 from typing import Literal
 import sys, warnings
+import pandas as pd 
+import math
+import torch.optim as opt
 
 
+
+warnings.simplefilter('always', UserWarning)
 
 
 class BaseWorkflow:
     """
-    Implements the base workflow for running any of the pipelines. 
+    Implements the base workflow for running any of the pipelines.
     """
 
     # Static variable for optimizer choice
@@ -27,10 +32,13 @@ class BaseWorkflow:
         'milestones': [1e10]
     }
 
+    # Static list of keys allowed in optim args
+    OPT_LIST = ['optimizer', 'optim_args', 'gamma', 'milestones', 'lr', 'eps', 'betas']
+
     # Constructor
     def __init__(self, 
                  anndata : ad.AnnData, 
-                 config : Literal["cross-condition", "interpretable"] = "cross-condition", 
+                 config : Literal["cross-condition", "interpretable"] = "cross-condition",
                  verbose : bool = False):
 
         # Set internals
@@ -40,6 +48,7 @@ class BaseWorkflow:
         self.batch_key = None
         self.levels = None
         self.model_type = None
+
 
         if self.verbose: print(f'Initialized workflow to run {config} model.')
 
@@ -64,51 +73,79 @@ Model: {self.model_type}
     # Data prep - private call
     def _prep_data(self, 
                   factors : list, 
-                  batch_key : str = None):
+                  batch_key : str = None,
+                  minibatch_size : int = 128):
         """
         Creates the required data objects to run the model       
         """
-        try:
-            # Set params
+
+        if self.model_type is not None: # Flag cleared, proceed to data setup
             self.label_style = 'concat' if self.model_type != 'SCANVI' else 'one-hot'
             self.factors = factors
-            self.len_attrs = [len(pd.get_dummies(metadata[factor]).columns) for factor in factors]
+            self.len_attrs = [len(pd.get_dummies(self.anndata.obs[factor]).columns) for factor in factors]
             self.batch_key = batch_key
+            self.minibatch_size = minibatch_size
     
 
-            if self.verbose: print(f'\nCondition classes : {factors}\nNumber of attributes per class : {len_attrs}')
+            if self.verbose: print(f'\nCondition classes : {self.factors}\nNumber of attributes per class : {self.len_attrs}')
 
             
             # Handle batch & create the datasets
             if self.batch_key is not None:
-                self.dataset, self.levels, self.converter, self.batch_mapping = utils.construct_labels(self.anndat.X, self.anndat.obs, self.factors, style=self.label_style, batch_key=self.batch_key)
-                self.train_set, self.test_set, self.train_loader, self.test_loader, self.l_mean, self.l_scale = utils.distrib_dataset(dataset, 
-                                                                 levels, 
-                                                                 batch_size=batch_size,
+                self.dataset, self.levels, self.converter, self.batch_mapping = utils.construct_labels(self.anndata.X, self.anndata.obs, self.factors, style=self.label_style, batch_key=self.batch_key)
+                self.train_set, self.test_set, self.train_loader, self.test_loader, self.l_mean, self.l_scale = utils.distrib_dataset(self.dataset, 
+                                                                 self.levels, 
+                                                                 batch_size=128,
                                                                  batch_key=self.batch_key)
 
                 self.batch_correction = True
 
             # If no batch
             else:
-                self.dataset, self.levels, self.converter = utils.construct_labels(self.anndat.X, self.anndat.obs, self.factors, style=self.label_style)
-                self.train_set, self.test_set, self.train_loader, self.test_loader, self.l_mean, self.l_scale = utils.distrib_dataset(dataset, 
-                                                                 levels, 
-                                                                 batch_size=batch_size,
+                self.dataset, self.levels, self.converter = utils.construct_labels(self.anndata.X, self.anndata.obs, self.factors, style=self.label_style)
+                self.train_set, self.test_set, self.train_loader, self.test_loader, self.l_mean, self.l_scale = utils.distrib_dataset(self.dataset, 
+                                                                 self.levels, 
+                                                                 batch_size=128,
                                                                  batch_key=self.batch_key)
 
                 self.batch_correction = False
         
 
-        # Not supposed to access this from outside normally
-        except AttributeError :
-            sys.exit("ERROR: There seems to be no model registered to the workflow.")
-            
+        else:
+            warnings.warn("ERROR: There seems to be no model registered to the workflow. Make sure not to run this function directly if you did so. You must instead run the 'prep_model()' function.")
 
+    
+    def _fetch_model_args(self, model_args):
+        """
+        Used to modify model_args efficiently depending on the model used.
+        """
+        # For all models
+        model_args["reconstruction"] = self.reconstruction
+        model_args["batch_correction"] = self.batch_correction
+
+        # Model specific
+        match self.model_type:
+            case "SCANVI":
+                model_args["num_labels"] = math.prod(self.len_attrs)
+         
+            case "Patches":
+                model_args["num_labels"] = sum(self.len_attrs)
+                model_args["len_attrs"] = self.len_attrs
+        
+        return model_args
+
+    
+    def _clear_bad_optim_args(self):
+        """
+        Clears optim_args keys that won't be used downstream.
+        """
+        self.optim_args = {k : v for k,v in self.optim_args.items() if k in self.OPT_LIST}
+            
     
     def prep_model(self, 
                    factors : list,
                    batch_key : str = None,
+                   minibatch_size : int = 128,
                    model_type : Literal["SCVI", "SCANVI", "Patches"] = "Patches",
                    model_args : dict = None, 
                    optim_args : dict = None):
@@ -119,42 +156,60 @@ Model: {self.model_type}
 
         # Register model type
         self.model_type = model_type
-        self.reconstruction = "ZINB" if self.config == cross_condition else "ZINB_LD"
+        self.reconstruction = "ZINB" if self.config == 'cross_condition' else "ZINB_LD"
 
         # Prepare the data
-        self._prep_data(factors, batch_key)
+        self._prep_data(factors, batch_key, minibatch_size)
 
         # Grab model constructor
         constructor = getattr(models.scvi_variants, self.model_type)
+        
+        ## Additional inputs for models
+        
+        if model_args is None: model_args = {}  ### Get one if not provided
+
+        model_args = self._fetch_model_args(model_args)
+      
+        # Construct model
         try:
-            self.model = constructor(self.anndata.X.shape[-1], self.l_mean, self.l_scale, batch_correction = self.batch_correction, reconstruction=self.reconstruction, **model_args)
-
+            self.model = constructor(self.anndata.X.shape[-1], self.l_mean, self.l_scale, **model_args)
+            
         except Exception as e:
-            warnings.warn("WARNING: model_args were not parsed correctly, returning to model defaults...")
+            warnings.warn("\nINFO: model_args ignored, using model defaults...")
+            model_args = self._fetch_model_args({})
+            self.model = constructor(self.anndata.X.shape[-1], self.l_mean, self.l_scale, **model_args)
 
-        if self.verbose: print(f'Initialized {self.model_type} model.')
+
+        if self.verbose: print(f'\nInitialized {self.model_type} model.\nModel arguments: {model_args}')
+
+        # Get optimizer args if not provided
+        if optim_args is None: optim_args = {}
         
         # Fill in optimizer gaps
-        for key in OPT_DEFAULTS.keys():
+        for key in self.OPT_DEFAULTS.keys():
             if key not in optim_args.keys():
-                optim_args[key] = OPT_DEFAULTS[key]
+                optim_args[key] = self.OPT_DEFAULTS[key]
 
         # Grab model optimizer
         match self.model_type:
-            case self.model_type if self.model_type in OPT_CLASS1 :
+            case self.model_type if self.model_type in self.OPT_CLASS1 :
                 self.optim_args = {
                     'optimizer': opt.Adam, 
                     'optim_args': {'lr': optim_args['lr'], 'eps' : optim_args['eps'], 'betas' : optim_args['betas']}, 
                     'gamma': optim_args['gamma'], 
-                    'milestones': optim_args['milestones']}
+                    'milestones': optim_args['milestones']
+                }
 
             
-            case self.model_type if self.model_type in OPT_CLASS2 :
+            case self.model_type if self.model_type in self.OPT_CLASS2 :
                 self.optim_args = {
                     'lr': optim_args['lr'], 'eps' : optim_args['eps'], 'betas' : optim_args['betas']
                 }
 
-        if self.verbose: print(f'Optimizer args parsed successfully.')
+        # Clear whatever is unused
+        self._clear_bad_optim_args()
+
+        if self.verbose: print(f'\nOptimizer args parsed successfully. Final arguments: {self.optim_args}')
 
             
         
