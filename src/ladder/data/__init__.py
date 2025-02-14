140e11fb293677d4a59e4ac41164eb65fc4cf58b
zgr2788 <ozgur.beker@gmail.com>
2025-02-14 17:09:25 -0500

    fix new data module imports

--- src/ladder/data/__init__.py
+++ src/ladder/data/__init__.py
@@ -1,8 +1,8 @@
 from .builtin_data import get_data
 from .real_data import (
     AnndataConverter,
+    ConditionalDataset,
     MetadataConverter,
     construct_labels,
     distrib_dataset,
-    preprocess_anndata,
 )
