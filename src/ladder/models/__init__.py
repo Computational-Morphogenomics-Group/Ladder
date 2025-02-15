934be27555f29f57f66c75e671137f739a1c0e7e
zgr2788 <ozgur.beker@gmail.com>
2025-02-14 19:39:04 -0500

    start revamping models with mixin approach, finished MLP variants

--- src/ladder/models/__init__.py
+++ src/ladder/models/__init__.py
@@ -1,2 +1,1 @@
-from .basics import _broadcast_inputs, _make_fc, _make_func, _split_in_half
-from .scvi_variants import SCANVI, SCVI, Patches
+from . import torch_models
---
+++ src/ladder/models/torch_models/__init__.py
@@ -0,0 +1,11 @@
+from .MLP_variants import (
+    MLP,
+    ZINBMLP,
+    GaussianMLP,
+    LognormalMLP,
+    _ForwardMixin,
+    _GaussianMixin,
+    _LognormalMixin,
+    _MLPMixin,
+    _ZINBMixin,
+)
